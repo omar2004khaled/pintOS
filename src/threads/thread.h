fb0160d536fb69aa23bfd8f00cb671701d364992
omar2004khaled <omar2004khaled@gmail.com>
2025-04-19 12:03:02 +0000

    Implement timer_sleep with sleep queue

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -1,10 +1,10 @@
 #ifndef THREADS_THREAD_H
 #define THREADS_THREAD_H
-#include "threads/synch.h"
 
 #include <debug.h>
 #include <list.h>
 #include <stdint.h>
+#include "threads/synch.h"
 
 /* States in a thread's life cycle. */
 enum thread_status
@@ -90,12 +90,10 @@ struct thread
     uint8_t *stack;                     /* Saved stack pointer. */
     int priority;                       /* Priority. */
     struct list_elem allelem;           /* List element for all threads list. */
-
-
-    int64_t wake_tick;               // When this thread should wake up
-    struct semaphore wakeUp;     // Per-thread sleep semaphore
-    struct list_elem sleep_elem;     // List element for sleep_queue
-    
+ 
+    int64_t wake_tick;
+    struct semaphore sleep_sema;
+    struct list_elem sleep_elem;
     
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
